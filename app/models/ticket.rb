b5e8834f0739165699de58efee44b21891733fc6
Mohamad Abras <mohamad.abras@gmail.com>
2015-11-25 13:27:11 +0000

    Display a comment's state transition

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,6 +3,7 @@ class Ticket < ActiveRecord::Base
   validates :description, presence: true, length: { minimum: 10 }
   belongs_to :project
   belongs_to :author, class_name: "User"
+  belongs_to :state
   has_many :attachments, dependent: :destroy
   has_many :comments, dependent: :destroy
   accepts_nested_attributes_for :attachments, reject_if: :all_blank
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,10 @@ end
                    )
   end
 end
+
+unless State.exists?
+  State.create(name: "New", color: "#0066CC")
+  State.create(name: "Open", color: "#008000")
+  State.create(name: "Closed", color: "#990000")
+  State.create(name: "Awesome", color: "#663399")
+end
